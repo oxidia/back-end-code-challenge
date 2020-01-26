5c7ce2c46d6a6543cef96e07b036dfb44a4977ec
Youssef Bahlaoui <ybahlaou@student.1337.ma>
2020-01-27 00:00:59 +0100

    Refactored to handle promise

--- api/controllers/repositories.js
+++ api/controllers/repositories.js
@@ -1,10 +1,14 @@
 const { prepareLanguages } = require('../utils');
 
-exports.languages = (req, res, next) => {
-  const languages = prepareLanguages();
+exports.languages = async (req, res, next) => {
+  try {
+    const languages = await prepareLanguages();
 
-  res.status(200).send({
-    count: languages.length,
-    data: languages
-  });
+    res.status(200).send({
+      count: languages.length,
+      data: languages
+    });
+  } catch (error) {
+    next(error);
+  }
 };
