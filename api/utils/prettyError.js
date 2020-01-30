9dec280daedadfa6fcb51abe445a882614c0451b
bahlaouiyoussef <byoussef773@gmail.com>
2020-01-30 13:05:34 +0100

    Fix repeated field error

--- api/utils/prettyError.js
+++ api/utils/prettyError.js
@@ -2,17 +2,18 @@ module.exports = error => {
   const errors = [];
   const needlePattern = /"[^"]*" /;
 
-  console.log(error);
   error.details.forEach(item => {
-    errors.push({
-      field: item.context.key,
-      message: item.message.replace(needlePattern, '')
-    });
+    if (!errors.find(error => error.field === item.context.key)) {
+      errors.push({
+        field: item.context.key,
+        message: item.message.replace(needlePattern, '')
+      });
+    }
   });
 
   return {
     message: 'Validation failed',
     errors,
-    documentation_url: 'https://domain_name.io/api/v1'
+    documentation_url: '/'
   };
 };
